07e84f800c559615a87c1e8f064a5bd5aa616a46
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-11-18 01:44:30 -0300

    development class car

--- poo/car.py
+++ poo/car.py
@@ -1,4 +1,34 @@
 
 
-class Car():
-    pass
+class Car:
+
+    def __init__(self, color, qtd_doors, type_fuel, power, qtd_fuel, is_on):
+        self.color = color
+        self.qtd_doors = qtd_doors
+        self.type_fuel = type_fuel
+        self.power = power
+        self.qtd_fuel = qtd_fuel
+        self.is_on = is_on
+
+    #abastercer
+    def tofuel(self):
+        self.qtd_fuel += 20
+
+    #ligar
+    def  turnon(self):
+        if self.is_on:
+            print('the car is on!')
+        else:
+            self.is_on = True
+
+    def turnoff(self):
+        if not self.is_on:
+            print('the car is off')
+        else:
+            self.is_on = False
+
+    def verify_batery(self):
+        pass
+
+    def verify_board_computer(self):
+        pass
