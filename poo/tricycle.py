e4524a3f04d841d323beec0e5edffd40bead9895
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-12-24 16:23:50 -0300

    settings

--- poo/tricycle.py
+++ poo/tricycle.py
@@ -1,4 +1,4 @@
-import poo.vehicle as vehicle, poo.motorcycle as motorcycle
+import poo.motorcycle as motorcycle
 
 # multiple herance,
 # about inherits the father class,  see below!
@@ -7,5 +7,5 @@ class Tricycle(motorcycle.Motorcycle):
     def __init__(self, *args):
         super(Tricycle, self).__init__(*args)
 
-instante_tricycle = Tricycle(2,'GREEN', 'GASOLINE', '500').velocity = 2
-print(instante_tricycle)
+tricycle = Tricycle(2,'GREEN', 'GASOLINE', '500').velocity = 2
+print(tricycle)
