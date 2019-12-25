e01ffd588db17820592d0201368b2ca046add943
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-12-25 00:41:59 -0300

    working with private attributes

--- poo/vehicle.py
+++ poo/vehicle.py
@@ -4,12 +4,12 @@ class Vehicle:
     """
 
     def __init__(self, color: str, type_fuel: str, power: int):
-        self.color = color
-        self.type_fuel = type_fuel
-        self.power = power
-        self.qtd_fuel: int = 0
-        self.is_on: bool = False
-        self.velocity: int = 0
+        self.__color = color
+        self.__type_fuel = type_fuel
+        self.__power = power
+        self.__qtd_fuel: int = 0
+        self.__is_on: bool = False
+        self.__velocity: int = 0
 
     def __del__(self):
         print('object has been removed from memory!')
@@ -20,26 +20,26 @@ class Vehicle:
         :param qtd_fuel: responsible for increasing a certain amount of fuel
         :return:
         """
-        self.qtd_fuel += qtd_fuel
+        self.__qtd_fuel += qtd_fuel
 
     # ligar
     def turn_on(self):
-        if self.is_on:
+        if self.__is_on:
             print('the vehicle is already on!!!')
         else:
-            self.is_on = True
+            self.__is_on = True
             print('the vehicle is on!')
 
     def turn_off(self):
-        if not self.is_on:
+        if not self.__is_on:
             print('the vehicle is already off!!')
         else:
-            self.is_on = False
+            self.__is_on = False
             print('the vehicle is off')
 
     def speed_up(self, velocity: int = 10):
-        if self.is_on:
-            self.velocity += velocity
+        if self.__is_on:
+            self.__velocity += velocity
         else:
             print('to speed up the vehicle needs to be on!!')
 
